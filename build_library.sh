9a9fd8981ee101223816055680c979993aff0553
kimminkyeu <minkyeki@student.42seoul.kr>
2023-07-12 23:53:54 +0900

    Initial Commit

--- build_library.sh
+++ build_library.sh
@@ -5,13 +5,13 @@ mkdir ./lib
 
 # build glew
 make -C ./glew
-cp -r -f ./glew/tmp ./lib/glew
-cp -r -f ./glew/lib ./lib/glew
-cp -r -f ./glew/bin ./lib/glew
-cp -r -f ./glew/glew.pc ./lib/glew/glew.pc
+cp -r -f ./GLEW/tmp ./lib/GLEW
+cp -r -f ./GLEW/lib ./lib/GLEW
+cp -r -f ./GLEW/bin ./lib/GLEW
+cp -r -f ./GLEW/glew.pc ./lib/GLEW/glew.pc
 # build된 파일만 쏙! 나머지 자원은 정리.
-make clean -C ./glew
+make clean -C ./GLEW
 
 # build glfw
-cmake -S ./glfw -B ./lib/glfw/build
-make -C ./lib/glfw/build
+cmake -S ./GLFW -B ./lib/GLFW/build
+make -C ./lib/GLFW/build
