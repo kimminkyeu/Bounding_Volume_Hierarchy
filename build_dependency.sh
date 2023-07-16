1e7cf5071334d0f04e9ea721c1ae001a4503811a
kimminkyeu <minkyeki@student.42seoul.kr>
2023-07-16 15:47:16 +0900

    feat: add script to pre-install dependency by CMakeLists.txt

--- build_dependency.sh
+++ build_dependency.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-
 # (1) on Ubuntu, install build tools first.
 # ------------------------------------------------
 # sudo apt-get install build-essential libxmu-dev libxi-dev libgl-dev libglu-dev
 
-
-# (2) on MacOS, ??
+# (2) on MacOS
 # ------------------------------------------------
-# 이건 클러스터에서 테스트 진행할 것.
+# ...
 
-
-# build GLEW
+# (3) build GLEW
 # https://github.com/nigels-com/glew
 # ------------------------------------------------
-# (1) build with make
+# (3-1) build with make
 	# make -C ./GLEW
 	# sudo make install -C ./GLEW  #시스템에 직접 GLEW 설치
 	# make clean -C ./GLEW
 	
-# (2) build with cmake
+# (3-2) build with cmake
 cd ./dependency/GLEW/build
 cmake ./cmake
 make glew_s # build the glew static library
 cd ../../../
 
 
-# build GLFW with cmake
+# (4) build GLFW
 # ------------------------------------------------
 cmake -S ./dependency/GLFW -B ./dependency/GLFW/build
 make -C ./dependency/GLFW/build
