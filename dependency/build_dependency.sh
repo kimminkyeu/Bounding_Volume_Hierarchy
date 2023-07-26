98e8ade5715c397ecac1fcadf15e75276f89eb20
kimminkyeu <minkyeki@student.42seoul.kr>
2023-07-26 16:39:45 +0900

    fix: change cmakeList

--- dependency/build_dependency.sh
+++ dependency/build_dependency.sh
@@ -4,8 +4,13 @@
   cd ./GLEW/build
   cmake ./cmake && make glew_s # build the glew static library
   cd ../../
+
 # (2) build GLFW      # https://github.com/glfw/glfw
   cmake -S ./GLFW -B ./GLFW/build && make -C ./GLFW/build
+
 # (3) build spdlog    # https://github.com/gabime/spdlog
   cmake -S ./spdlog -B ./spdlog/build && make -C ./spdlog/build -j
+
+# (4) build assimp    # https://github.com/assimp/assimp
+  cmake -S ./assimp -B ./assimp/build && make -C ./assimp/build -j
 
