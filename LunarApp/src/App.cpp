4d72cb16658780ed0ae7e8fb608fcb48ecf5617e
kimminkyeu <minkyeki@student.42seoul.kr>
2023-09-24 13:13:24 +0900

    feat: Texture Paint mode GUI setup

--- LunarApp/src/App.cpp
+++ LunarApp/src/App.cpp
@@ -278,6 +278,12 @@ public:
 		}
 	}
 
+	enum class ButtonToggleFlags
+	{
+		LayoutMode			= 0,
+		TexturePaintMode	= 1,
+		RenderMode			= 1 << 1,
+	};
 
 	// NOTE: this is ImGui Render function
     void OnUIRender() override
@@ -305,17 +311,18 @@ public:
             // Split the dock node in the left direction to create our first docking space. This will be on the left side of the node.
             // (The 0.5f means that the new space will take up 50% of its parent - the dock node.)
             ImGuiID dock0 = ImGui::DockBuilderSplitNode(id, ImGuiDir_Up, 0.07f, nullptr, &id);
-            ImGuiID dock1 = ImGui::DockBuilderSplitNode(id, ImGuiDir_Left, 0.7f, nullptr, &id);
+			ImGuiID dock2 = ImGui::DockBuilderSplitNode(id, ImGuiDir_Right, 0.2f, nullptr, &id);
+            ImGuiID dock1_a = ImGui::DockBuilderSplitNode(id, ImGuiDir_Left, 0.5f, nullptr, &id);
+			ImGuiID dock1_b = ImGui::DockBuilderSplitNode(dock1_a, ImGuiDir_Left, 0.5f, nullptr, &dock1_a);
             // +-----------+
-            // |           |
+			// +-----------+
             // |     1     |
             // |           |
             // +-----------+
             // Split the same dock node in the right direction to create our second docking space.
             // At this point, the dock node has two spaces, one on the left and one on the right.
-            ImGuiID dock2 = ImGui::DockBuilderSplitNode(id, ImGuiDir_Right, 0.5f, nullptr, &id);
-            // +-----+-----+
-            // |     |     |
+			// +-----------+
+			// +-----+-----+
             // |  1  |  2  |
             // |     |     |
             // +-----+-----+
@@ -325,7 +332,6 @@ public:
             //
             // Notice how "dock2" is now passed rather than "id".
             // The new space takes up 50% of the second space rather than 50% of the original dock node.
-            ImGuiID dock3 = ImGui::DockBuilderSplitNode(id, ImGuiDir_Down, 0.9f, nullptr, &dock2);
             // +-----+-----+
             // |     |  2  |  split
             // |  1  +-----+    |
@@ -334,9 +340,10 @@ public:
 
             // 3. Add windows to each docking space:
             ImGui::DockBuilderDockWindow("Mode Selection", dock0);
-            ImGui::DockBuilderDockWindow("Main Viewport", dock1);
-            ImGui::DockBuilderDockWindow("WINDOW_B_TOP", dock2);
-            ImGui::DockBuilderDockWindow("WINDOW_B_BOTTOM", dock3);
+            ImGui::DockBuilderDockWindow("Main Viewport", dock1_a);
+			ImGui::DockBuilderDockWindow("Second Viewport", dock1_b);
+            ImGui::DockBuilderDockWindow("Property", dock2);
+//            ImGui::DockBuilderDockWindow("Property", dock3);
 
             // 4. We're done setting up our docking configuration:
             ImGui::DockBuilderFinish(id);
@@ -352,72 +359,99 @@ public:
 
         const ImVec4 CLICKED_COLOR = ImVec4{0.4f, 0.5f, 0.8f, 1.0f};
         const ImVec4 DEFAULT_COLOR = ImVec4{0.2f, 0.3f, 0.4f, 1.0f};
-        #define PUSH_STYLE_COLOR(state, btnIdx) \
+        #define LUNAR_PUSH_STYLE_COLOR(state, btnIdx) \
                 ((state == btnIdx)              \
                 ? ImGui::PushStyleColor(ImGuiCol_Button, CLICKED_COLOR) \
                 : ImGui::PushStyleColor(ImGuiCol_Button, DEFAULT_COLOR));
 
 
-        static int ToggledBtn = 0; // default button is 0 (Layout)
+        static ButtonToggleFlags ToggledBtn; // default button is 0 (Layout)
 
-        PUSH_STYLE_COLOR(ToggledBtn, 0);
+		LUNAR_PUSH_STYLE_COLOR(ToggledBtn, ButtonToggleFlags::LayoutMode);
         ImGui::SameLine(0.0, 2.0f);
         if (ImGui::Button("Layout", ImVec2(100, 20)))
         {
-            ToggledBtn = 0;
+			ToggledBtn = ButtonToggleFlags::LayoutMode;
             m_RayTracingMode = false;
         }
         ImGui::PopStyleColor(1);
 
-        PUSH_STYLE_COLOR(ToggledBtn, 1);
+		LUNAR_PUSH_STYLE_COLOR(ToggledBtn, ButtonToggleFlags::TexturePaintMode);
         ImGui::SameLine(0.0, 2.0f);
         if (ImGui::Button("Texture Paint", ImVec2(130, 20)))
         {
-            ToggledBtn = 1;
+            ToggledBtn = ButtonToggleFlags::TexturePaintMode;
             m_RayTracingMode = false;
         }
         ImGui::PopStyleColor(1);
 
-        PUSH_STYLE_COLOR(ToggledBtn, 2);
+		LUNAR_PUSH_STYLE_COLOR(ToggledBtn, ButtonToggleFlags::RenderMode);
         ImGui::SameLine(0.0, 2.0f);
         if (ImGui::Button("Rendering", ImVec2(100, 20)))
         {
-            ToggledBtn = 2;
+            ToggledBtn = ButtonToggleFlags::RenderMode;
             m_RayTracingMode = true;
         }
         ImGui::PopStyleColor(1);
-
         ImGui::End();
 
         /* **************************************
          * Set content of each dock.
          * **************************************/
+		#define LUNAR_PUSH_IMAGE_TO_VIEWPORT(targetDockNode_str, image_texture) 									 \
+					ImGui::Begin((targetDockNode_str)); 														 \
+					m_GUIScreenSize = ImGui::GetContentRegionAvail(); 											 \
+					ImGui::Image( 																				 \
+							reinterpret_cast<void *>((image_texture)), 											 \
+							m_GUIScreenSize, 																	 \
+							ImVec2(0, 1), 																		 \
+							ImVec2(1, 0)                                                       					 \
+							); 																					 \
+					m_EditorCamera->OnResize(m_GUIScreenSize.x, m_GUIScreenSize.y); 							 \
+					ImGui::End();
+
+
 		const auto currentShaderName = m_DisplayMode.GetCurrentShaderPtr()->GetName();
 		{
-			ImGui::Begin("WINDOW_B_TOP");
-			if (ToggledBtn == 2) // **************************************************************************
+			if (ToggledBtn == ButtonToggleFlags::RenderMode) // **************************************************************************
 			{
+				LUNAR_PUSH_IMAGE_TO_VIEWPORT("Main Viewport", m_RayTracer.GetFinalImageFrameBuffer()->GetFrameTexture());
+//				ImGui::Begin("Main Viewport");
+//				m_GUIScreenSize = ImGui::GetContentRegionAvail();
+//				ImGui::Image(
+//						reinterpret_cast<void *>(m_RayTracer.GetFinalImageFrameBuffer()->GetFrameTexture()),
+//						m_GUIScreenSize,
+//						ImVec2(0, 1),
+//						ImVec2(1, 0)
+//				);
+//				m_EditorCamera->OnResize(m_GUIScreenSize.x, m_GUIScreenSize.y);
+//				ImGui::End();
+
+				ImGui::Begin("Property");
 				ImGui::BeginGroup();
 				{
 					ImGui::Text("Last render: %.3fms", m_RayTracer.m_LastRenderTime);
 					ImGui::Checkbox("Moller-Trumbore", &m_RayTracer.m_ChangeIntersection);
 				}
 				ImGui::EndGroup();
-
-				ImGui::Begin("Main Viewport");
-                m_GUIScreenSize = ImGui::GetContentRegionAvail();
-				ImGui::Image(
-                        reinterpret_cast<void *>(m_RayTracer.GetFinalImageFrameBuffer()->GetFrameTexture()),
-                        m_GUIScreenSize,
-                        ImVec2(0, 1),
-                        ImVec2(1, 0)
-				);
-				// NOTE: 윈도우 사이즈는 동일하지만 ImGUI Viewport 사이즈가 바뀌었을 경우
-				m_EditorCamera->OnResize(m_GUIScreenSize.x, m_GUIScreenSize.y);
 				ImGui::End();
 			}
-			else // shader, GPU mode. ***************************************************************************
+			else if (ToggledBtn == ButtonToggleFlags::LayoutMode) // shader, GPU mode. ***************************************************************************
 			{
+				LUNAR_PUSH_IMAGE_TO_VIEWPORT("Main Viewport", m_RasterizationFrameBuffer.GetFrameTexture());
+//				ImGui::Begin("Main Viewport");
+//				m_GUIScreenSize = ImGui::GetContentRegionAvail(); // https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples
+//				ImGui::Image(
+//						reinterpret_cast<void *>(m_RasterizationFrameBuffer.GetFrameTexture()),
+//						m_GUIScreenSize,
+//						ImVec2(0, 1),
+//						ImVec2(1, 0)
+//				);
+//				m_EditorCamera->OnResize(m_GUIScreenSize.x, m_GUIScreenSize.y);
+//				ImGui::End();
+
+				// Material // https://github.com/TheCherno/RayTracing/blob/master/RayTracing/src/WalnutApp.cpp
+				ImGui::Begin("Property");
 				// https://uysalaltas.github.io/2022/01/09/OpenGL_Imgui.html
 				if (ImGui::BeginMenu(currentShaderName.c_str()))
 				{
@@ -429,93 +463,83 @@ public:
 					}
 					ImGui::EndMenu();
 				}
+				ImGui::BeginGroup();
 				{
-					ImGui::Begin("WINDOW_B_BOTTOM");
+					ImGui::Checkbox("Grid", &m_DataVisualizer.m_ShowGrid);
+					ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowGrid ? "On" : "Off");
+					ImGui::Checkbox("Mesh", &m_ShowMesh);
+					ImGui::SameLine(); ImGui::Text("%s", m_ShowMesh ? "On" : "Off");
+					ImGui::Checkbox("Edge", &m_DataVisualizer.m_ShowWireframe);
+					ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowWireframe ? "On" : "Off");
+					ImGui::Checkbox("Vetex", &m_DataVisualizer.m_ShowPoint);
+					ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowPoint ? "On" : "Off");
+					ImGui::Checkbox("Normal", &m_DataVisualizer.m_ShowNormal);
+					ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowNormal ? "On" : "Off");
+					ImGui::Checkbox("AABB", &m_DataVisualizer.m_ShowAABB);
+					ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowAABB ? "On" : "Off");
+				}
+				ImGui::EndGroup();
+				ImGui::BeginGroup();
+				{
+					if (currentShaderName == "Phong" || currentShaderName == "Cartoon")
 					{
-						// Material // https://github.com/TheCherno/RayTracing/blob/master/RayTracing/src/WalnutApp.cpp
-						ImGui::BeginGroup();
-						{
-							ImGui::Checkbox("Grid", &m_DataVisualizer.m_ShowGrid);
-							ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowGrid ? "On" : "Off");
-							ImGui::Checkbox("Mesh", &m_ShowMesh);
-							ImGui::SameLine(); ImGui::Text("%s", m_ShowMesh ? "On" : "Off");
-							ImGui::Checkbox("Edge", &m_DataVisualizer.m_ShowWireframe);
-							ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowWireframe ? "On" : "Off");
-							ImGui::Checkbox("Vetex", &m_DataVisualizer.m_ShowPoint);
-							ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowPoint ? "On" : "Off");
-							ImGui::Checkbox("Normal", &m_DataVisualizer.m_ShowNormal);
-							ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowNormal ? "On" : "Off");
-							ImGui::Checkbox("AABB", &m_DataVisualizer.m_ShowAABB);
-							ImGui::SameLine(); ImGui::Text("%s", m_DataVisualizer.m_ShowAABB ? "On" : "Off");
-						}
-						ImGui::EndGroup();
-						ImGui::BeginGroup();
-						{
-							if (currentShaderName == "Phong" || currentShaderName == "Cartoon")
-							{
-								ImGui::ColorEdit3("Ambient Color", glm::value_ptr(m_Model.Material.m_AmbientColor));
-								ImGui::ColorEdit3("Diffuse Color", glm::value_ptr(m_Model.Material.m_DiffuseColor));
-								ImGui::ColorEdit3("Specular Color", glm::value_ptr(m_Model.Material.m_SpecularColor));
-								ImGui::DragFloat("Specular Exponent", &m_Model.Material.m_SpecularExponent);
-							}
-							else if (currentShaderName == "Explosion")
-							{
-								auto* ptr = dynamic_cast<ExplosionShader *>(m_DisplayMode.GetCurrentShaderPtr());
-								if (ptr != nullptr) {
-									ImGui::SliderFloat("Explosion Degree", &(ptr->m_Degree), 0.0f, 10.0f, "%.1f");
-								}
-							}
-							if (m_DataVisualizer.m_ShowWireframe)
-							{
-								auto* ptr = dynamic_cast<WireframeShader *>(m_DataVisualizer.m_WireframeShader);
-								if (ptr != nullptr) {
-									ImGui::ColorEdit3("Wireframe Color", glm::value_ptr(ptr->m_WireframeColor));
-								}
-							}
-							if (m_DataVisualizer.m_ShowPoint)
-							{
-								auto* ptr = dynamic_cast<PointShader *>(m_DataVisualizer.m_PointShader);
-								if (ptr != nullptr) {
-									ImGui::ColorEdit3("Point Color", glm::value_ptr(ptr->m_PointColor));
-								}
-							}
-							if (m_DataVisualizer.m_ShowAABB)
-							{
-								auto* ptr = dynamic_cast<AABBShader*>(m_DataVisualizer.m_AABBShader);
-								if (ptr != nullptr) {
-									ImGui::ColorEdit3("AABB Color", glm::value_ptr(ptr->m_AABBColor));
-									ImGui::SliderInt("AABB Depth", &m_BBoxDebugDrawLevel, 0, static_cast<int>(m_AABB->m_MaxDepth));
-								}
-							}
-							if (m_DataVisualizer.m_ShowGrid)
-							{
-								auto* ptr = dynamic_cast<GridShader*>(m_DataVisualizer.m_GridShader);
-								if (ptr != nullptr) {
-									ImGui::ColorEdit3("Grid Color", glm::value_ptr(ptr->m_GridColor));
-								}
-							}
-						}
-						ImGui::EndGroup();
+						ImGui::ColorEdit3("Ambient Color", glm::value_ptr(m_Model.Material.m_AmbientColor));
+						ImGui::ColorEdit3("Diffuse Color", glm::value_ptr(m_Model.Material.m_DiffuseColor));
+						ImGui::ColorEdit3("Specular Color", glm::value_ptr(m_Model.Material.m_SpecularColor));
+						ImGui::DragFloat("Specular Exponent", &m_Model.Material.m_SpecularExponent);
 					}
-					ImGui::End();
+					else if (currentShaderName == "Explosion")
+					{
+						auto* ptr = dynamic_cast<ExplosionShader *>(m_DisplayMode.GetCurrentShaderPtr());
+						if (ptr != nullptr) {
+							ImGui::SliderFloat("Explosion Degree", &(ptr->m_Degree), 0.0f, 10.0f, "%.1f");
+						}
+					}
+					if (m_DataVisualizer.m_ShowWireframe)
+					{
+						auto* ptr = dynamic_cast<WireframeShader *>(m_DataVisualizer.m_WireframeShader);
+						if (ptr != nullptr) {
+							ImGui::ColorEdit3("Wireframe Color", glm::value_ptr(ptr->m_WireframeColor));
+						}
+					}
+					if (m_DataVisualizer.m_ShowPoint)
+					{
+						auto* ptr = dynamic_cast<PointShader *>(m_DataVisualizer.m_PointShader);
+						if (ptr != nullptr) {
+							ImGui::ColorEdit3("Point Color", glm::value_ptr(ptr->m_PointColor));
+						}
+					}
+					if (m_DataVisualizer.m_ShowAABB)
+					{
+						auto* ptr = dynamic_cast<AABBShader*>(m_DataVisualizer.m_AABBShader);
+						if (ptr != nullptr) {
+							ImGui::ColorEdit3("AABB Color", glm::value_ptr(ptr->m_AABBColor));
+							ImGui::SliderInt("AABB Depth", &m_BBoxDebugDrawLevel, 0, static_cast<int>(m_AABB->m_MaxDepth));
+						}
+					}
+					if (m_DataVisualizer.m_ShowGrid)
+					{
+						auto* ptr = dynamic_cast<GridShader*>(m_DataVisualizer.m_GridShader);
+						if (ptr != nullptr) {
+							ImGui::ColorEdit3("Grid Color", glm::value_ptr(ptr->m_GridColor));
+						}
+					}
 				}
-				{
-					ImGui::Begin("Main Viewport");
-                    m_GUIScreenSize = ImGui::GetContentRegionAvail();
-					// NOTE: put m_FrameBuffer's image data to ImGui Image
-					// https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples
-					ImGui::Image(
-                            reinterpret_cast<void *>(m_RasterizationFrameBuffer.GetFrameTexture()),
-                            m_GUIScreenSize,
-                            ImVec2(0, 1),
-                            ImVec2(1, 0)
-					);
-					// NOTE: 윈도우 사이즈는 동일하지만 ImGUI Viewport 사이즈가 바뀌었을 경우
-					m_EditorCamera->OnResize(m_GUIScreenSize.x, m_GUIScreenSize.y);
-					ImGui::End();
-				}
+				ImGui::EndGroup();
+				ImGui::End();
 			}
-			ImGui::End();
+			else if (ToggledBtn == ButtonToggleFlags::TexturePaintMode)
+			{
+				LUNAR_PUSH_IMAGE_TO_VIEWPORT("Main Viewport", m_RasterizationFrameBuffer.GetFrameTexture());
+
+				ImGui::Begin("Second Viewport");
+				// ...
+				ImGui::End();
+
+				ImGui::Begin("Property");
+				// ...
+				ImGui::End();
+			}
 		}
 	}
 
@@ -536,7 +560,7 @@ public:
 
 Lunar::Application* Lunar::CreateApplication(int argc, char** argv) noexcept
 {
-	Lunar::ApplicationSpecification spec {"Scoop", 1300, 900 };
+	Lunar::ApplicationSpecification spec {"Scoop", 1800, 1000 };
 	auto* app = new Lunar::Application(spec);
     app->PushLayer<ExampleLayer>();
 	app->SetMenubarCallback([app]()-> void
